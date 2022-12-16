773a0d17a995b840aca656da082c0a81d1bf7ff4
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2022-12-17 00:54:15 +0100

    added navbar effect

--- script.js
+++ script.js
@@ -46,3 +46,16 @@ window.addEventListener('click', function(e)
 )
 
 
+// navbar
+let navContainer = document.getElementById('menu')
+let links = navContainer.getElementsByClassName('btn')
+
+for (let i = 0; i < links.length; i++){
+  links[i].addEventListener('click', function(){
+    let current = document.getElementsByClassName('active')
+    current[0].className = current[0].className.replace(' active')
+    this.className += ' active'
+  })
+}
+
+
