54adce9cd3c65dc8d228710949557dd276ca1d4b
Rasheedat Jinadu <jinaduleoluwa@gmal.com>
2023-03-07 22:16:25 +0100

    yay! I cleaned my javascript code

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-let accordion = document.getElementsByClassName('box-container')
+let accordion = document.querySelectorAll(".box-container");
 
-for(i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener('click',
-        function(){
-            this.classList.toggle('active')
-        }
-    )
-}
+accordion.forEach((question) => {
+  question.addEventListener("click", () => {
+    question.classList.toggle("active");
+  });
+});
 
-let navContainer = document.getElementById('menu')
-let links = navContainer.getElementsByClassName('btn')
+let navContainer = document.getElementById("menu");
+let links = navContainer.getElementsByClassName("btn");
 
-for(i = 0; i < links.length; i++){
-    links[i].addEventListener('click', function(){
-        let current = document.getElementsByClassName('active')
-        current[0].className = current[0].className.replace(' active')
-        this.className += ' active'
-    })
+for (i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active");
+    this.className += " active";
+  });
 }
--- script.js
+++ script.js
@@ -1,61 +1,41 @@
-let modal = document.querySelector('.modal');
-let poolModal = document.querySelector('.modal-talent');
-let openBtn = document.querySelector('#myBtn')
-let openBtn2 = document.querySelector('#myBtn2')
-let openBtn3 = document.querySelector('#myBtn3')
-let poolOpenBtn = document.querySelector('#poolModalBtn')
-let poolOpenBtn2 = document.querySelector('#poolModalBtn2')
+let modal = document.querySelector(".modal");
+let modalBtn = document.querySelectorAll(".pop");
 
-openBtn.addEventListener('click',
-    function(){
-      modal.style.display ='flex'
-      modal.style.backgroundColor ='rgba(0, 0, 0, 0.7)'
-    }
-)
-openBtn2.addEventListener('click',
-    function(){
-      modal.style.display ='flex'
-      modal.style.backgroundColor ='rgba(0, 0, 0, 0.7)'
-    }
-)
-openBtn3.addEventListener('click',
-    function(){
-      modal.style.display ='flex'
-      modal.style.backgroundColor ='rgba(0, 0, 0, 0.7)'
-    }
-)
+let poolModal = document.querySelector(".modal-talent");
+let grayModalBtn = document.querySelectorAll(".pop2");
 
-poolOpenBtn.addEventListener('click', function(){
-  poolModal.style.display = 'flex'
-  poolModal.style.backgroundColor ='rgba(0, 0, 0, 0.7)'
-})
-poolOpenBtn2.addEventListener('click', function(){
-  poolModal.style.display = 'flex'
-  poolModal.style.backgroundColor ='rgba(0, 0, 0, 0.7)'
-})
+modalBtn.forEach((popUp) =>
+  popUp.addEventListener("click", () => {
+    popUp.classList.add("pop");
+    modal.style.display = "flex";
+    modal.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  })
+);
 
-window.addEventListener('click', function(e)
-  {
-    const target = e.target
-    if(e.target === modal){
-      modal.style.display = 'none'
-    }else if(e.target === poolModal){
-      poolModal.style.display = 'none'
-    }
+grayModalBtn.forEach((grayBtn) => {
+  grayBtn.addEventListener("click", () => {
+    poolModal.style.display = "flex";
+    poolModal.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  });
+});
+
+window.addEventListener("click", function (e) {
+  const target = e.target;
+  if (e.target === modal) {
+    modal.style.display = "none";
+  } else if (e.target === poolModal) {
+    poolModal.style.display = "none";
   }
-)
-
+});
 
 // navbar
-let navContainer = document.getElementById('menu')
-let links = navContainer.getElementsByClassName('btn')
+let navContainer = document.getElementById("menu");
+let links = navContainer.getElementsByClassName("btn");
 
-for (let i = 0; i < links.length; i++){
-  links[i].addEventListener('click', function(){
-    let current = document.getElementsByClassName('active')
-    current[0].className = current[0].className.replace(' active')
-    this.className += ' active'
-  })
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active");
+    this.className += " active";
+  });
 }
-
-
